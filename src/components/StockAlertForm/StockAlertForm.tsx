3207db625aadeded874fdd23f22d84f61bb8ca9a
Felix Navas <navasfx19@gmail.com>
2024-01-29 14:22:34 -0600

    ✨Render combobox options from API and add submit handler

--- src/components/StockAlertForm/StockAlertForm.tsx
+++ src/components/StockAlertForm/StockAlertForm.tsx
@@ -1,67 +1,81 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from 'react';
+
+import Alert from '@mui/material/Alert';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import InputAdornment from '@mui/material/InputAdornment';
 import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
 import OutlinedInput from '@mui/material/OutlinedInput';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-
+import TextField from '@mui/material/TextField';
 import { Typography } from '@mui/material';
 
-const stockSymbols = [
-  {
-    stockSymbol: 'BTC',
-  },
-  {
-    stockSymbol: 'S&P',
-  },
-  {
-    stockSymbol: 'MMM',
-  },
-  {
-    stockSymbol: 'APL',
-  },
-];
+import { useStockSymbols } from '../../_hooks/useStockSymbols/useStockSymbols';
+import { StockDataInfo } from '../../services/httpClient';
+
+const filterOptions = createFilterOptions({
+  matchFrom: 'start',
+  stringify: (option: StockDataInfo) => option.symbol,
+  limit: 200,
+});
 
 export const StockAlertForm = () => {
+  const { data: stockOptions, error, loading } = useStockSymbols();
+
+  const [formValues, setFormValues] = useState({
+    priceAlert: 0,
+    stockSymbol: '',
+  });
+
+  useEffect(() => {
+    console.log(formValues);
+  }, [formValues]);
+
+  const onAddStock = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setFormValues({
+      priceAlert: Number((e.target as any).priceAlert.value),
+      stockSymbol: (e.target as any).stockSymbol.value,
+    });
+  };
+
   return (
     <Box sx={{ p: 1 }}>
       <Typography variant='h6'>Set your alert</Typography>
-      <form>
+      <form onSubmit={onAddStock}>
         <div>
           <FormControl sx={{ m: 1, mt: 2, width: '10rem' }} size='small'>
-            <InputLabel htmlFor='price-alert'>Price Alert</InputLabel>
+            <InputLabel htmlFor='priceAlert'>Price Alert</InputLabel>
             <OutlinedInput
-              id='price-alert'
+              id='priceAlert'
               startAdornment={
                 <InputAdornment position='start'>$</InputAdornment>
               }
               label='Price Alert'
             />
           </FormControl>
-          <FormControl sx={{ m: 1, width: '10rem' }} size='small'>
-            <InputLabel id='stock-symbol-select-label'>Stock</InputLabel>
-            <Select
-              labelId='stock-symbol-select-label'
-              id='stock-symbol-select'
-              label='Stock'
-              onChange={(e: SelectChangeEvent) => console.log(e.target.value)}
-            >
-              {stockSymbols.map(({ stockSymbol }) => (
-                <MenuItem key={stockSymbol} value={stockSymbol}>
-                  {stockSymbol}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+          <Autocomplete
+            id='stockSymbol'
+            size='small'
+            loading={loading}
+            options={stockOptions}
+            getOptionLabel={(option) => option.symbol}
+            filterOptions={filterOptions}
+            sx={{ m: 1, width: '10rem' }}
+            renderInput={(params) => <TextField {...params} label='Stock' />}
+          />
           <Button
             type='submit'
             variant='contained'
+            disabled={error || loading}
             sx={{ m: 1, width: '10rem' }}
           >
             Add Stock
           </Button>
+          {error ?? <Alert severity='error'>Something went wrong!</Alert>}
         </div>
       </form>
     </Box>
