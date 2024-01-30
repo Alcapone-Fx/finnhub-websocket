15c48e4bbbf5bf34f8f3cb98befd208b85e8ae26
Felix Navas <navasfx19@gmail.com>
2024-01-30 13:34:42 -0600

    💄Add CSS normalizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
