139d97789fdefad40b3beac524bad6582005a9e1
Felix Navas <navasfx19@gmail.com>
2024-01-30 13:33:32 -0600

    🔥Remove unused code

--- src/components/StockAlertForm/StockAlertForm.tsx
+++ src/components/StockAlertForm/StockAlertForm.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from 'react';
+import React from 'react';
 
 import Alert from '@mui/material/Alert';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
@@ -15,68 +15,63 @@ import { Typography } from '@mui/material';
 import { useStockSymbols } from '../../_hooks/useStockSymbols/useStockSymbols';
 import { StockDataInfo } from '../../services/httpClient';
 
+type StockAlertFormProps = {
+  onAddStock: (symbol: string, priceAlert: number) => void;
+};
+
 const filterOptions = createFilterOptions({
   matchFrom: 'start',
   stringify: (option: StockDataInfo) => option.symbol,
   limit: 200,
 });
 
-export const StockAlertForm = () => {
+export const StockAlertForm: React.FC<StockAlertFormProps> = ({
+  onAddStock,
+}) => {
   const { data: stockOptions, error, loading } = useStockSymbols();
 
-  const [formValues, setFormValues] = useState({
-    priceAlert: 0,
-    stockSymbol: '',
-  });
-
-  useEffect(() => {
-    console.log(formValues);
-  }, [formValues]);
-
-  const onAddStock = (e: React.FormEvent<HTMLFormElement>) => {
+  const addStock = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    setFormValues({
-      priceAlert: Number((e.target as any).priceAlert.value),
-      stockSymbol: (e.target as any).stockSymbol.value,
-    });
+    onAddStock(
+      (e.target as any).stockSymbol.value,
+      Number((e.target as any).priceAlert.value)
+    );
   };
 
   return (
-    <Box sx={{ p: 1 }}>
-      <Typography variant='h6'>Set your alert</Typography>
-      <form onSubmit={onAddStock}>
-        <div>
-          <FormControl sx={{ m: 1, mt: 2, width: '10rem' }} size='small'>
-            <InputLabel htmlFor='priceAlert'>Price Alert</InputLabel>
-            <OutlinedInput
-              id='priceAlert'
-              startAdornment={
-                <InputAdornment position='start'>$</InputAdornment>
-              }
-              label='Price Alert'
-            />
-          </FormControl>
-          <Autocomplete
-            id='stockSymbol'
-            size='small'
-            loading={loading}
-            options={stockOptions}
-            getOptionLabel={(option) => option.symbol}
-            filterOptions={filterOptions}
-            sx={{ m: 1, width: '10rem' }}
-            renderInput={(params) => <TextField {...params} label='Stock' />}
+    <Box>
+      <Typography variant='h6' sx={{ pl: 1 }}>
+        Set your alert
+      </Typography>
+      <form onSubmit={addStock}>
+        <FormControl sx={{ m: 1, mt: 2, width: '10rem' }} size='small'>
+          <InputLabel htmlFor='priceAlert'>Price Alert</InputLabel>
+          <OutlinedInput
+            id='priceAlert'
+            startAdornment={<InputAdornment position='start'>$</InputAdornment>}
+            label='Price Alert'
           />
-          <Button
-            type='submit'
-            variant='contained'
-            disabled={error || loading}
-            sx={{ m: 1, width: '10rem' }}
-          >
-            Add Stock
-          </Button>
-          {error ?? <Alert severity='error'>Something went wrong!</Alert>}
-        </div>
+        </FormControl>
+        <Autocomplete
+          id='stockSymbol'
+          size='small'
+          loading={loading}
+          options={stockOptions}
+          getOptionLabel={(option) => option.symbol}
+          filterOptions={filterOptions}
+          sx={{ m: 1, width: '10rem' }}
+          renderInput={(params) => <TextField {...params} label='Stock' />}
+        />
+        <Button
+          type='submit'
+          variant='contained'
+          disabled={error || loading}
+          sx={{ m: 1, width: '10rem' }}
+        >
+          Add Stock
+        </Button>
+        {error ?? <Alert severity='error'>Something went wrong!</Alert>}
       </form>
     </Box>
   );
