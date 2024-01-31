e1502b994ee07a91d1628d6f4d5d7fba4622d7c7
Felix Navas <navasfx19@gmail.com>
2024-01-31 11:13:42 -0600

    ✨Enable PWA app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'PWA Finnhub WS',
+        short_name: 'PWA Finnhub WS',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    commonjsOptions: { transformMixedEsModules: true },
+  },
 });
